9d24d231beb67b76d646e7cb2151462d2e34e02d
Mark Christian Mercado <markchristian113@gmail.com>
2025-12-17 15:08:58 +0800

    fixed token expiration checking

--- src/main/java/com/merufureku/aromatica/collection_service/constants/CollectionConstants.java
+++ src/main/java/com/merufureku/aromatica/collection_service/constants/CollectionConstants.java
@@ -4,5 +4,7 @@ public class CollectionConstants {
 
     public static final String ACCESS_TOKEN = "ACCESS";
 
+    public static final int ACCESS_TOKEN_EXPIRATION_MINUTES = 15;
+
     public static final String COLLECTION_SERVICE = "collection_service";
 }
